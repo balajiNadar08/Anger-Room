d19f672274d3acd902b2df83a637e8cacf0f3af5
balajiNadar08 <balajigittttt@gmail.com>
2025-08-24 19:05:13 +0530

    Added footer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Feature from "@/components/Feature";
 import Testimonial from "@/components/Testimonial";
-import { User, Globe, Lock } from 'lucide-react';
+import { User, Globe, Lock, Github, Instagram } from "lucide-react";
 
 export default function Home() {
   return (
@@ -36,17 +36,17 @@ export default function Home() {
         </div>
       </div>
 
-
       <div className="flex items-center justify-center w-full px-4 md:px-6 lg:px-8">
-        <div className="bg-gradient-to-tl from-[#FFF] to-[#00A19C] h-64 md:h-80 lg:h-[40rem] 
-        w-full md:w-[85%] lg:w-[70%] rounded-xl md:rounded-2xl"></div>
+        <div
+          className="bg-gradient-to-tl from-[#FFF] to-[#00A19C] h-64 md:h-80 lg:h-[40rem] 
+        w-full md:w-[85%] lg:w-[70%] rounded-xl md:rounded-2xl"
+        ></div>
       </div>
 
-
-      <div className="flex flex-col items-center pt-24 md:pt-30 lg:pb-8 px-4 md:px-8 lg:px-8">
+      <div className="flex flex-col items-center pt-24 md:pt-30 lg:pb-8 px-4 md:px-8">
         <div className="max-w-4xl md:pb-4 text-center">
           <p className="text-2xl md:text-3xl font-bold pb-2 leading-tight">
-            Features That Let You Rant Freely
+            Features To Rant Freely 🗣️🔥
           </p>
           <p className="text-gray-400 text-sm md:text-base lg:text-lg">
             A safe and anonymous space to share your thoughts with the world.
@@ -55,19 +55,19 @@ export default function Home() {
 
         <div className="max-w-5xl pt-8 grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <Feature
-            title="Login" 
+            title="Login"
             content="Join instantly as a guest (anonymous) or sign in with google account for a more personal experience."
           >
             <User />
           </Feature>
           <Feature
-            title="Global Chatroom" 
+            title="Global Chatroom"
             content="Shared space where everyone's messages appear in real time, creating a sense of community ranting!"
           >
             <Globe />
           </Feature>
           <Feature
-            title="Safe Space" 
+            title="Safe Space"
             content="A platform designed for people to freely share feelings, thoughts and frustrations without fear of judgment."
           >
             <Lock />
@@ -75,8 +75,7 @@ export default function Home() {
         </div>
       </div>
 
-      
-      <div className="flex flex-col items-center pt-20 md:pt-24 pb-16 md:pb-20 lg:pb-50 px-4 md:px-8">
+      <div className="flex flex-col items-center pt-20 px-4 md:px-8">
         <div className="max-w-4xl md:pb-4 text-center">
           <p className="text-2xl md:text-3xl font-bold pb-2 leading-tight">
             The After-Rant Glow ✨
@@ -86,22 +85,71 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="max-w-5xl pt-8 mb-30 grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <Testimonial 
+        <div className="max-w-5xl pt-8 pb-18 grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <Testimonial
             name="Aarav R."
             content="Whenever college stress builds up, I hop on AngerRoom. Ten minutes of ranting here feels better than an hour of complaining to friends."
             imagePath="/assets/pfp1.webp"
           />
-          <Testimonial 
+          <Testimonial
             name="Zoya K."
             content="Honestly, I didn't expect typing out my anger would feel so freeing. It's like yelling into the void, but in a good way. I actually laughed at the end!"
             imagePath="/assets/pfp2.webp"
           />
-          <Testimonial 
+          <Testimonial
             name="Aditya M."
             content="I love how private it feels. I can rage about work, exams or random stuff and then move on with a clear head. Highly recommend!"
             imagePath="/assets/pfp3.webp"
           />
+        </div>
+      </div>
+
+      <div className="flex justify-center md:pt-4 pb-16 md:pb-20 lg:pb-20 px-4 md:px-8">
+        <div className="max-w-5xl lg:w-full rounded-xl py-10 flex flex-col gap-8 md:gap-14 items-center bg-gradient-to-tl from-[#FFF] to-[#00A19C]">
+          <div className="flex flex-col gap-4 md:gap-6 text-center text-[#0A0A0A]">
+            <h2 className="text-3xl md:text-4xl font-semibold">
+              Ready to Rant?
+            </h2>
+            <p className="md:text-xl">
+              Join hundreds of users connecting every day. <br />
+              Create your guest account or sign up now and experience seamless
+              communication.
+            </p>
+          </div>
+          <button
+            className="text-[#0A0A0A] text-base md:text-lg font-semibold rounded-full border-2 border-gray-800 bg-gradient-to-r from-white to-[#00A19C] 
+            py-3 px-8 md:py-3 md:px-10 cursor-pointer hover:-translate-y-1 transition-all ease-in-out duration-500
+            active:scale-95 touch-manipulation"
+          >
+            Start Ranting 😁
+          </button>
+        </div>
+      </div>
+
+      <div className="flex flex-col md:items-center px-8 pb-4">
+        <div className="max-w-5xl md:w-full pt-6 border-t-2 border-gray-600">
+          <div className="flex flex-col lg:flex-row justify-between gap-8">
+            <p className="text-3xl font-bold">AngerRoom</p>
+
+            <div className="pb-10 lg:flex lg:gap-8">
+              <p className="pb-2 text-lg lg:text-xl">DEVELOPER'S SOCIAL</p>
+              <div className="flex gap-2 lg:gap-4">
+                <a href="https://github.com/balajiNadar08" target="_blank">
+                  <Github size={26} />
+                </a>
+                <a
+                  href="https://www.instagram.com/balaji_nadar_08/"
+                  target="_blank"
+                >
+                  <Instagram size={26} />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <p className="text-lg text-center text-gray-400">
+            © 2025 AngerRoom. All rights reserved.
+          </p>
         </div>
       </div>
     </div>
